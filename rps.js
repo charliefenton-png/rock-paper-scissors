c0045d866e1e1d28ac84b145b54ae7709e42a7c7
charliefenton-png <307664431+charliefenton-png@users.noreply.github.com>
2026-09-11 20:31:46 +0100

    first try

--- rps.js
+++ rps.js
@@ -7,6 +7,14 @@ const scissors = "scissors"
 
 function getComputerChoice(Math.floor(Math.random() * 3)); {
 // if 0 then give rock, else if 1 give paper, else if 2 give scissors //
+  if (0) {
+    console.log(rock); 
+  } else if (1) {
+      console.log(paper)
+  } else {
+      console.log(scissors)
+  }
+
 }
 
 
