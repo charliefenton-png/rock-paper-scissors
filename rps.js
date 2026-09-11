49c32f674213b059021a8a54c2a6ea0444f3c4e4
charliefenton-png <307664431+charliefenton-png@users.noreply.github.com>
2026-09-11 20:24:38 +0100

    started computer choice function

---
+++ rps.js
@@ -0,0 +1,32 @@
+
+
+const rock = "rock"
+const paper = "paper"
+const scissors = "scissors"
+
+
+function getComputerChoice(Math.floor(Math.random() * 3)); {
+// if 0 then give rock, else if 1 give paper, else if 2 give scissors //
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Hello World")
