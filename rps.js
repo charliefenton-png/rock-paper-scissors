0eb702cda1cb3c4facf36a842f30b5dee20c14c4
charliefenton-png <307664431+charliefenton-png@users.noreply.github.com>
2026-09-13 15:30:07 +0100

    restarting getcomputerchoice function

--- rps.js
+++ rps.js
@@ -3,22 +3,28 @@
 const rock = "rock"
 const paper = "paper"
 const scissors = "scissors"
-const getComputerChoice = "getComputerChoice"
 
 
-// if 0 then give rock, else if 1 give paper, else if 2 give scissors //
+/* Create a new function named getComputerChoice.
+Write the code so that getComputerChoice 
+will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+Hint: The Math.random method returns a random number that’s greater than or equal to 0
+and less than 1. Think about how you can use this
+to conditionally return one of the multiple choices. */
 
-function (math.random)( * 3); {
 
-  if (0) {
-    console.log(rock); 
-  } else if (1) {
-      console.log(paper)
-  } else {
-      console.log(scissors)
-  }
 
-}
+
+
+
+
+
+
+
+
+
+
+
 
 
 console.log(getComputerChoice)
