21c364e06db29f387f295d6e829e83e48dcd0e13
charliefenton-png <307664431+charliefenton-png@users.noreply.github.com>
2026-09-14 19:22:56 +0100

    started playRound function

--- rps.js
+++ rps.js
@@ -58,3 +58,37 @@ Initialize those variables with the value of 0. */
 
 let humanScore = 0
 let computerScore = 0
+
+
+
+
+
+/* Your game will be played round by round. 
+You will write a function that takes the human and computer player choices as arguments,
+plays a single round, increments the round winner’s score and logs a winner announcement.
+Create a new function named playRound.
+Define two parameters for playRound: humanChoice and computerChoice.
+Use these two parameters to take the human and computer choices as arguments.
+Make your function’s humanChoice parameter case-insensitive
+so that players can input “rock”, “ROCK”, “RocK”, or other variations.
+Write the code for your playRound function to console.log a string value representing the round winner,
+such as: “You lose! Paper beats Rock”.
+Increment the humanScore or computerScore variable based on the round winner. */
+
+
+// make new function called playRound //
+// define two parameters for playRound: humanChoice and computerChoice //
+// use humanChoice and computerChoice to take the human and computer choices as arguments //
+// make function's humanChoice parameter case-insensitive //
+/* write code for playRound function to console.log a string value 
+     representing the round winner */
+// increment the humanScore or computerScore variable base on the round winner //     
+
+
+
+function playRound(humanChoice,computerChoice) {
+     
+}
+
+
+
