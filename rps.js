0184a1f9807950ebffcd8b9306aaeeae7813f289
charliefenton-png <307664431+charliefenton-png@users.noreply.github.com>
2026-09-16 20:56:03 +0100

    another redo of playround function

--- rps.js
+++ rps.js
@@ -1,74 +1,19 @@
-
-
-/* Create a new function named getComputerChoice.
-Write the code so that getComputerChoice 
-will randomly return one of the following string values: “rock”, “paper” or “scissors”.
-Hint: The Math.random method returns a random number that’s greater than or equal to 0
-and less than 1. Think about how you can use this
-to conditionally return one of the multiple choices. */
-
-
-
-// create function called getComputerChoice //
-// getComputeChoice randomly return one of following string values "rock", "paper" or "scissors" //
-// use Math.random method to conditionally return of of "rock", "paper" or "scissors" //
-
-
-const rock = "rock";
-const paper = "paper";
-const scissors = "scissors";
 const options = ['rock','paper','scissors']
 
-
+let humanScore = 0
+let computerScore = 0
 
 
 function getComputerChoice() {
   return options[Math.floor(Math.random()*options.length)]; 
-  }
-
-
-
-
-/* Create a new function named getHumanChoice.
-Write the code so that getHumanChoice will return one of the valid choices
-depending on what the user inputs.
-Hint: Use the prompt method to get the user’s input.
-You do not need to handle reprompting if the user enters an invalid input, 
-as that would require things we will teach later. For now, 
-just assume the user will always enter a valid choice.
-Test what your function returns by using console.log. */
-
-
-
-// create function getHumanChoice //
-/* make getHumanChoice return rock, paper or scissors using prompt method to return
-     user input choice */
-// assume user will always enter valid choice, don't handle reprompting //
-// test what getHumanChoice returns with console.log //   
-
+}
+  
 
 
 function getHumanChoice() {
   const choice = window.prompt("rock, paper or scissors?");
   return choice.toLowerCase();
 }
-
-
-
-
-
-/* Declare the players score variables
-Your game will keep track of the players score. 
-You will write variables to keep track of the players score.
-Create two new variables named humanScore and computerScore in the global scope.
-Initialize those variables with the value of 0. */
-
-
-let humanScore = 0
-let computerScore = 0
-
-
-
 
 
 /* Your game will be played round by round. 
@@ -93,11 +38,31 @@ Increment the humanScore or computerScore variable based on the round winner. */
 // increment the humanScore or computerScore variable base on the round winner //     
 
 
-const humanChoice = console.log(getHumanChoice() + " humans choice");
-const computerChoice = console.log(getComputerChoice()+ " computers choice");
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
 
 function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "It's a tie!";
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        return "You win!";
+    } else {
+        return "You lose!";
+    }
 }
+
+
+
+
+
+
+console.log(playRound(humanChoice, computerChoice));
+
   
 
 
