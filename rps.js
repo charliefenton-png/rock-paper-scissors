072a9d7ecfe88139349b470ce38cbfd0edf36a3f
charliefenton-png <307664431+charliefenton-png@users.noreply.github.com>
2026-09-17 16:57:00 +0100

    added instruction for step 6

--- rps.js
+++ rps.js
@@ -18,27 +18,6 @@ function getHumanChoice() {
 }
 
 
-/* Your game will be played round by round. 
-You will write a function that takes the human and computer player choices as arguments,
-plays a single round, increments the round winner’s score and logs a winner announcement.
-Create a new function named playRound.
-Define two parameters for playRound: humanChoice and computerChoice.
-Use these two parameters to take the human and computer choices as arguments.
-Make your function’s humanChoice parameter case-insensitive
-so that players can input “rock”, “ROCK”, “RocK”, or other variations.
-Write the code for your playRound function to console.log a string value representing the round winner,
-such as: “You lose! Paper beats Rock”.
-Increment the humanScore or computerScore variable based on the round winner. */
-
-
-// make new function called playRound //
-// define two parameters for playRound: humanChoice and computerChoice //
-// use humanChoice and computerChoice to take the human and computer choices as arguments //
-// make function's humanChoice parameter case-insensitive //
-/* write code for playRound function to console.log a string value 
-     representing the round winner */
-// increment the humanScore or computerScore variable based on the round winner //     
-
 
 const humanChoice = getHumanChoice();
 const computerChoice = getComputerChoice();
@@ -69,7 +48,24 @@ console.log(`computerScore: ${computerScore}`);
 
 
 
+/* Step 6: Write the logic to play the entire game
+Your game will play 5 rounds. You will write a function named playGame
+ that calls playRound to play 5 rounds, 
+ keeps track of the scores and declares a winner at the end.
 
+Create a new function named playGame.
+Move your playRound function and score variables
+ so that they’re declared inside of the new playGame function
+Play 5 rounds by calling playRound 5 times.
+Hint: When you assign a function call to a variable, 
+the return value of that function is assigned to the variable. 
+Accessing the variable afterward will only provide the assigned value; 
+it doesn’t recall the function. You need to 
+recall the choice functions to get new choices for each round.
+Re-work your previous functions or create more helper functions if necessary.
+ Specifically, you may want to change the return values to something more useful.
+If you already know about loops, you can use them. If not, don’t worry! 
+Loops will be covered in the next lesson.  */
 
   
 
